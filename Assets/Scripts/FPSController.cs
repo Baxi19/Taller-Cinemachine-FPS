6a8c6356c8db1d13690e53105ae6a7f016621aec
Baxi19 <randald1991@gmail.com>
2021-05-08 13:51:53 -0600

    Testing cinemachine

---
+++ Assets/Scripts/FPSController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    CharacterController characterController;
+    public float walkSpeed = 6.0f;
+    public float runSpeed = 10.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private Vector3 move = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                move = transform.TransformDirection(move) * runSpeed;
+            }
+            else
+            {
+                move = transform.TransformDirection(move) * walkSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                move.y = jumpSpeed;
+            }
+        }
+        move.y -= gravity * Time.deltaTime;
+        characterController.Move(move * Time.deltaTime);
+    }
+}
